e4610671b667c0bd3609859cdd68199261f1cdc0
Hoàng Huy <146901880+DoHuy123@users.noreply.github.com>
2023-10-05 13:12:15 +0700

    bai3

---
+++ Lab1_Bai1.3/Find.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Bai1._3
+{
+    internal class Find
+    {
+        public T FindMax<T>(IEnumerable<T> sequence)// IEnumrable la mot giao dien dai dien cho mot tap hop có the lap qua cac phan tu 
+        where T : IComparable<T>  // cau lenh nay de su cung ToCompare() cho KDL <T> , where kiem soat KDL mà tham so generic co the nhan 
+        {
+            if (sequence == null || !sequence.Any()) // ham Any() kiem tra xem sequence co it nhat mot phan 
+            {
+                throw new ArgumentException("Sequence is empty or null.");
+            }
+
+            {
+                T max = sequence.First();
+                foreach (var item in sequence)
+                {
+                    if (item.CompareTo(max)>0) //***
+                    {
+                        max = item;
+                    }
+                }
+                return max;
+            }
+        }
+        public T FindMin<T>(IEnumerable<T> sequence)
+            where T : IComparable<T>
+        {
+            if (sequence ==null || !sequence.Any())
+            {
+                throw new ArgumentException(" Sequence is empty or null");
+
+            }
+            {
+                T min = sequence.First();
+                foreach (var item in sequence)
+                {
+                    if (item.CompareTo(min)< 0)
+                    {
+                        min = item;
+                    }
+
+
+                }
+                return min;
+            }
+
+        }
+    }
+}
---
+++ Lab1_Bai1.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Bai1._3
+{
+    internal class Program
+    {
+
+        
+        static void Main(string [] args)
+        {
+            List<string> list = new List<string> { "apple", "banana", "cherry", "date" };
+            ;
+            List<int> list2 = new List<int> { 3, 7, 1, 9, 5 }; ;
+            List<double> list3 = new List<double> {3.4, 5.6,2.1,6.2,4.8 };
+
+            Find find = new Find();
+
+            int maxInt = find.FindMax(list2);
+            double maxDouble = find.FindMax(list3);
+            string maxString = find.FindMax(list);
+
+            int minInt = find.FindMin(list2);
+            double minDouble = find.FindMin(list3);
+            string minString = find.FindMin(list);
+
+            Console.WriteLine($"Max int: {maxInt}");
+            Console.WriteLine($"Max double: {maxDouble}");
+            Console.WriteLine($"Max string: {maxString}");
+
+            Console.WriteLine($"Min int: {minInt}");
+            Console.WriteLine($"Min double: {minDouble}");
+            Console.WriteLine($"Min string: {minString}");
+
+        }
+    }
+}
