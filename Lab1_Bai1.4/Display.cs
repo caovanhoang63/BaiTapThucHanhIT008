85c90f2167819d020a8418d38bd7d9c654a488da
Hoàng Huy <146901880+DoHuy123@users.noreply.github.com>
2023-10-09 22:02:24 +0700

    bai 4

---
+++ Lab1_Bai1.4/Display.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Bai1._4
+{
+    internal class Display
+    {
+        public void Sub(Themometer themometer)
+        {
+            themometer.TemperaterChange +=HandleTemperatureChanged;
+        }
+        private void HandleTemperatureChanged(object sender, TempChangedEvent e)
+        {
+            TempChangedEvent temp = (TempChangedEvent)e;////
+            Console.WriteLine("New Temperature: {0}°C", temp.NewTemp);/////
+        }
+    }
+}
--- Lab1_Bai1.4/Program.cs
+++ Lab1_Bai1.4/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab1_Bai1._4
 {
+  
     internal class Program
     {
+
+       
         static void Main(string[] args)
         {
+            Themometer themometer = new Themometer();
+            Display display = new Display();
+            // dang ki man hinh hien thi voi nhiet ke
+            display.Sub(themometer);
+            themometer.MesureTemp();
         }
     }
 }
+
+
+     
